05d57d9f7526f3985a05181cc52ef823fa360dc6
Mirian Barajas <mirianbarcam@gmail.com>
2022-08-30 18:18:13 +0000

    endpoint token

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,9 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token,jwt_required,get_jwt_identity
+
+from datetime import timedelta
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +19,58 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/login', methods=['POST', 'GET'])
+def login():
+    email = request.json.get('email')
+    password = request.json.get('password')
+    
+    user = User.query.filter_by(email=email).first()
+    
+    if user:
+        if password !=  user.password:
+            return jsonify({'msg':'usuario o clave invalido'}),401
+        else:
+            time = timedelta(minutes = 1)
+            print(time)
+            access_token = create_access_token(identity=email,expires_delta=time)
+            return jsonify({"access_token":access_token, "duration": time.total_seconds()})
+    else:
+        return jsonify({'msg':'email invalida'}),401
+
+    response_body = {
+        "message": "login"
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/register', methods=['POST'])
+def register():
+    email = request.json.get('email')
+    password = request.json.get('password')
+    
+    userRegister = User.query.filter_by(email=email).first()
+    
+    if userRegister:
+        return jsonify({'msg':'email pertenece a otro usuario'}),401
+    else:
+        new_email = User(email=email,password=password,is_active=True)
+        db.session.add(new_email)
+        db.session.commit()
+        return jsonify({'msg':'registrado correctamente'}),200
+
+    response_body = {
+        "message": "login"
+    }
+
+    return jsonify(response_body), 200
+
+
+
+@api.route('/privada',methods=['GET'])
+@jwt_required()
+def protegida():
+    print("hola todo bien")
+    identidad = get_jwt_identity()
+    
+    return jsonify({'msg':'acceso correcto','identity':identidad}),200
